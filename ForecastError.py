8f36da922ec3820828099d2942b2b932830b1ff6
Robin <robin.thibaut@ugent.be>
2020-03-30 11:53:36 +0200

    Adapted kernel density

--- ForecastError.py
+++ ForecastError.py
@@ -26,7 +26,7 @@ res_dir = jp(bel_dir, 'objects')
 f_files = [jp(res_dir, f) for f in listdir(res_dir) if isfile(jp(res_dir, f)) and 'forecasts' in f]
 t_files = [jp(res_dir, f) for f in listdir(res_dir) if isfile(jp(res_dir, f)) and 'true' in f]
 
-s = 9
+s = 0
 true0 = np.load(t_files[s])
 forecast0 = np.load(f_files[s])
 shape = np.array(true0).shape
@@ -38,17 +38,59 @@ v = np.array([c0.allsegs[0][0] for c0 in c0s])
 # v = [p.vertices for p in ps]
 plt.close()
 
+x = np.hstack([vi[:, 0] for vi in v])
+y = np.hstack([vi[:, 1] for vi in v])
+
+xykde = np.vstack([x, y]).T
+
 # %% Kernel density
 
-plt.plot(v[1][:, 0], v[1][:, 1], 'o-')
+nn = 2
+plt.plot(v[nn][:, 0], v[nn][:, 1], 'o-')
 plt.show()
+
+# Sklearn
+
+xmin = 0
+xmax = 1500
+ymin = 0
+ymax = 1000
+xgrid = np.arange(xmin, xmax, 5)
+ygrid = np.arange(ymin, ymax, 5)
+X, Y = np.meshgrid(xgrid, ygrid)
+xy = np.vstack([X.ravel(), Y.ravel()]).T
+
+x0, y0, radius = 1000, 500, 200
+r = np.sqrt((xy[:, 0] - x0)**2 + (xy[:, 1] - y0)**2)
+inside = r < radius
+xyu = xy[inside]
+
+
+kde = KernelDensity(kernel='gaussian',
+                    bandwidth=1.5).fit(xykde)
+score = np.exp(kde.score_samples(xyu))
+z = np.full(inside.shape, 0, dtype=float)
+z[inside] = score
+
+z = np.flipud(z.reshape(shape))
+plt.imshow(z,
+           extent=(xmin, xmax, ymin, ymax),
+           cmap='coolwarm')
+plt.contour(mp.x, mp.y, true0, [0], colors='red')
+wells_xy = np.load(jp(cwd, 'grid', 'iw.npy'), allow_pickle=True)[:, :2]
+plt.plot(wells_xy[:, 0], wells_xy[:, 1], 'ko', alpha=1)
+plt.xlim(750, 1200)
+plt.ylim(300, 700)
+plt.show()
+
+levels = np.linspace(-500, z.max(), 25)
+plt.contourf(X, Y, z, cmap='coolwarm')
+plt.show()
+
 
 # Seaborn
 
 wells_xy = np.load(jp(cwd, 'grid', 'iw.npy'), allow_pickle=True)[:, :2]
-
-x = np.hstack([vi[:, 0] for vi in v])
-y = np.hstack([vi[:, 1] for vi in v])
 sns.kdeplot(x, y, cmap="coolwarm", shade=True, shade_lowest=False, cbar=True)
 plt.contour(mp.x, mp.y, true0, [0], colors='red')
 plt.plot(wells_xy[:, 0], wells_xy[:, 1], 'co', alpha=1)
@@ -58,13 +100,7 @@ plt.savefig(jp(bel_dir, 'comp.pdf'))
 plt.show()
 
 # Scipy
-xykde = np.vstack([x, y]).T
 kernel = stats.gaussian_kde(xykde)
-
-# Sklearn
-kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(xykde)
-scores = kde.score_samples(xykde)
-z = np.full(shape,0)
 
 # %% mean approach
 super_mean = np.mean(forecast0, axis=0)
