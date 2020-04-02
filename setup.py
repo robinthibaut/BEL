bcd6056e032674e4238b819f3170e2235253247a
Robin <robin.thibaut@ugent.be>
2020-04-02 12:46:51 +0200

    Polished

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Project',
+    version='',
+    packages=['bel', 'bel.bel', 'bel.hydro', 'bel.hydro.flow', 'bel.hydro.whpa', 'bel.hydro.sgems',
+              'bel.hydro.transport', 'bel.hydro.backtracking', 'bel.tools'],
+    url='',
+    license='',
+    author='Robin Thibaut',
+    author_email='',
+    description=''
+)
