a81624ac7f426aebfa745625daf6a3f1fef05938
Robin <robin.thibaut@ugent.be>
2020-04-07 11:04:42 +0200

    Polished

--- bel/temp/folder_cleanup.py
+++ bel/temp/folder_cleanup.py
@@ -1,5 +1,6 @@
 import os
 from os.path import join as jp
+import shutil
 
 cwd = os.getcwd()
 res_dir = jp('..', 'hydro', 'results')
@@ -8,3 +9,14 @@ for r, d, f in os.walk(res_dir, topdown=False):
     # Adds the data files to the lists, which will be loaded later
     if 'sd.npy' in f:
         os.remove(jp(r, 'sd.npy'))
+
+for r, d, f in os.walk(res_dir, topdown=False):
+    # Adds the data files to the lists, which will be loaded later
+    if 'bkt.npy' in f and 'sd.npy' in f and 'hk0.npy' in f:
+        pass
+    else:  # If one of the files is missing, deletes the sub-folder
+        try:
+            if r != res_dir:  # Make sure to not delete the main results directory !
+                shutil.rmtree(r)
+        except TypeError:
+            pass
