66cf78524a32ea1a837b9f8597d22e9d6d120940
Robin <robin.thibaut@ugent.be>
2020-04-15 10:27:27 +0200

    Minor changes

--- bel/hydro/simulation.py
+++ bel/hydro/simulation.py
@@ -12,16 +12,16 @@ from bel.hydro.transport.mt3d import transport
 from bel.hydro.whpa.travelling_particles import tsp
 from bel.toolbox.file_ops import FileOps
 
-# Directories
-mod_dir = os.getcwd()  # Module directory
-exe_loc = jp(mod_dir, 'exe')  # EXE directory
-# EXE files directory.
-exe_name_mf = jp(exe_loc, 'mf2005.exe')
-exe_name_mt = jp(exe_loc, 'mt3d.exe')
-exe_name_mp = jp(exe_loc, 'mp7.exe')
-
 
 def main(folder=None):
+    # Directories
+    mod_dir = os.getcwd()  # Module directory
+    exe_loc = jp(mod_dir, 'exe')  # EXE directory
+    # EXE files directory.
+    exe_name_mf = jp(exe_loc, 'mf2005.exe')
+    exe_name_mt = jp(exe_loc, 'mt3d.exe')
+    exe_name_mp = jp(exe_loc, 'mp7.exe')
+
     if not folder:
         # Main results directory.
         res_dir = uuid.uuid4().hex
