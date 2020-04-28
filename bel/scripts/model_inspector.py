bbb68b8090d25bc16764c3cba2490fdeb5967c49
Robin <robin.thibaut@ugent.be>
2020-04-28 14:29:56 +0200

    Minor changes

--- bel/scripts/model_inspector.py
+++ bel/scripts/model_inspector.py
@@ -16,64 +16,40 @@ from bel.hydro.backtracking.modpath import backtrack
 rn = 'example_illustration'
 results_dir = jp(os.getcwd(), 'bel', 'hydro', 'results', rn)
 
-# Load flow model
+# %% Load flow model
 m_load = jp(results_dir, 'whpa.nam')
 flow_model = fops.load_flow_model(m_load, model_ws=results_dir)
-flow_model.export(jp(results_dir, 'vtk', 'flow'), fmt='vtk')
-vtk.export_heads(flow_model, jp(results_dir, 'whpa.hds'), jp(results_dir, 'vtk', 'flow'), binary=True, kstpkper=(0, 0))
-# Wells are ordered by node number
-spd = flow_model.wel.stress_period_data.df
+delr = flow_model.modelgrid.delr
+delc = flow_model.modelgrid.delc
+xyz_vertices = flow_model.modelgrid.xyzvertices
+# I'll be working with hexahedron, vtk type = 12
+blocks2d = mops.blocks_from_rc(delc, delr)
+blocks = mops.blocks_from_rc_3d(delc, delr)
+blocks3d = blocks.reshape(-1, 3)
 
-# Load transport model
+
+def flow_vtk(export=False):
+    flow_model.export(jp(results_dir, 'vtk', 'flow'), fmt='vtk')
+    vtk.export_heads(flow_model,
+                     jp(results_dir, 'whpa.hds'),
+                     jp(results_dir, 'vtk', 'flow'),
+                     binary=True, kstpkper=(0, 0))
+
+
+# %% Load transport model
 mt_load = jp(results_dir, 'whpa.mtnam')
 transport_model = fops.load_transport_model(mt_load, flow_model, model_ws=results_dir)
-transport_model.export(jp(results_dir, 'vtk', 'transport'), fmt='vtk')
 ucn_files = [jp(results_dir, 'MT3D00{}.UCN'.format(i)) for i in range(1, 7)]
 ucn_obj = [flopy.utils.UcnFile(uf) for uf in ucn_files]
 times = [uo.get_times() for uo in ucn_obj]
 concs = np.array([uo.get_alldata() for uo in ucn_obj])
 
-# # let's take a look at our grid
-# fig = plt.figure(figsize=(8, 8))
-# ax = fig.add_subplot(1, 1, 1, aspect='equal')
-# mapview = flopy.plot.PlotMapView(model=flow_model, extent=(900, 1100, 400, 600))
-# ibound = mapview.plot_ibound()
-# linecollection = mapview.plot_grid()
-# wel = mapview.plot_bc("WEL")
-# plt.show()
-#
-# # let's take a look at our grid
-# fig_mt = plt.figure(figsize=(8, 8))
-# ax_mt = fig_mt.add_subplot(1, 1, 1, aspect='equal')
-# mapview_mt = flopy.plot.PlotMapView(model=transport_model, extent=(0, 1500, 0, 1000))
-# ibound_mt = mapview_mt.plot_ibound()
-# plt.show()
 
-# plot head
-# fname = jp(results_dir, 'whpa.hds')
-# hdobj = flopy.utils.HeadFile(fname)
-# head = hdobj.get_data()
-# # levels = np.arange(10, 30, .5)
+def transport_vtk():
+    transport_model.export(jp(results_dir, 'vtk', 'transport'), fmt='vtk')
 
-# fig = plt.figure(figsize=(15, 10))
-#
-# ax = fig.add_subplot(1, 2, 1, aspect='equal')
-# ax.set_title('plot_array()')
-# mapview = flopy.plot.PlotMapView(model=flow_model)
-# mapview.plot_ibound()
-# mapview.plot_array(head, masked_values=[999.], alpha=0.5)
-# mapview.plot_bc("WEL")
-# plt.show()
-#
-# ax = fig.add_subplot(1, 2, 2, aspect='equal')
-# ax.set_title('contour_array()')
-# mapview = flopy.plot.PlotMapView(model=flow_model)
-# mapview.plot_ibound()
-# mapview.plot_bc("WEL")
-# contour_set = mapview.contour_array(head, masked_values=[999.], levels=20)
-# plt.show()
 
-# Plot modpath
+# %% Plot modpath
 
 mp_reloaded = backtrack(flowmodel=flow_model, exe_name='', load=True)
 # load the endpoint data
@@ -106,19 +82,8 @@ mapview.plot_ibound()
 mapview.plot_endpoint(ept)
 plt.show()
 
-obs = fops.datread(jp(results_dir, 'MT3D001.OBS'), header=2)[:, 1:]
-plt.plot(obs[:, 0], obs[:, 1])
-plt.show()
 
-
-delr = flow_model.modelgrid.delr
-delc = flow_model.modelgrid.delc
-xyz_vertices = flow_model.modelgrid.xyzvertices
-# I'll be working with hexahedron, vtk type = 12
-blocks2d = mops.blocks_from_rc(delc, delr)
-blocks = mops.blocks_from_rc_3d(delc, delr)
-blocks3d = blocks.reshape(-1, 3)
-# Let's first try 2D export !
+# %% Export UCN to vtk
 cells = [("quad", np.array([list(np.arange(i*4, i*4+4))])) for i in range(len(blocks))]
 for i in range(1, 7):
     conc = np.array([np.fliplr(concs[i-1, j]).reshape(-1) for j in range(len(concs[i-1]))])
@@ -136,7 +101,7 @@ for i in range(1, 7):
         # field_data=field_data
         )
 
-
+# %%
 conc0 = np.abs(np.where(concs == 1e30, 0, concs))
 array = {}
 for i in range(1, 7):
@@ -154,7 +119,7 @@ meshio.write_points_cells(
     # field_data=field_data
     )
 
-
+# %%
 conc0 = np.abs(np.where(concs == 1e30, 0, concs))
 array = np.zeros(blocks.shape[0])
 for i in range(1, 7):
