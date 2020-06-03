61981990df62ef02a2dd91484240aace9a8fbea4
Robin <robin.thibaut@ugent.be>
2020-06-03 09:54:43 +0200

    Dimensionality check for Tarantola

--- experiment/math/postio.py
+++ experiment/math/postio.py
@@ -52,52 +52,62 @@ class PosteriorIO:
         """
 
         # TODO: add dimension check
-
+        if isinstance(h_cca_training_gaussian, (list, tuple, np.ndarray)):
+            shctg = np.shape(h_cca_training_gaussian)  # Shape = (n_components_CCA, n_training)
+        if isinstance(d_cca_training, (list, tuple, np.ndarray)):
+            sdct = np.shape(d_cca_training)  # Shape = (n_components_CCA, n_training)
+        if isinstance(d_pc_training, (list, tuple, np.ndarray)):
+            sdpt = np.shape(d_pc_training)  # Shape = (n_training, n_components_PCA)
+        if isinstance(d_rotations, (list, tuple, np.ndarray)):
+            sdr = np.shape(d_rotations)  # Shape = (n_components_PCA, n_components_CCA)
+        if isinstance(d_cca_prediction, (list, tuple, np.ndarray)):
+            sdcp = np.shape(d_cca_prediction)  # Shape = (n_components_CCA, 1)
 
         # Size of the set
         n_training = d_cca_training.shape[1]
 
         # Evaluate the covariance in h
-        h_cov_operator = np.cov(h_cca_training_gaussian.T, rowvar=False)  # same
+        h_cov_operator = np.cov(h_cca_training_gaussian.T, rowvar=False)  # (n_comp_CCA, n_comp_CCA)
 
         # Evaluate the covariance in d (here we assume no data error, so C is identity times a given factor)
         x_dim = np.size(d_pc_training, axis=1)  # Number of PCA components for the curves
         noise = .01
-        d_cov_operator = np.eye(x_dim) * noise  # I matrix.
-        d_noise_covariance = d_rotations.T @ d_cov_operator @ d_rotations  # same
+        d_cov_operator = np.eye(x_dim) * noise  # I matrix. (n_comp_PCA, n_comp_PCA)
+        d_noise_covariance = d_rotations.T @ d_cov_operator @ d_rotations  # (n_comp_CCA, n_comp_CCA)
 
         # Linear modeling d to h
         # Transpose to get same as Thomas
         g = np.linalg.lstsq(h_cca_training_gaussian.T, d_cca_training.T, rcond=None)[0].T
-        g = np.where(np.abs(g) < 1e-12, 0, g)
+        g = np.where(np.abs(g) < 1e-12, 0, g)  # (n_comp_CCA, n_comp_CCA)
 
         # Modeling error due to deviations from theory
-        d_ls_predicted = g @ h_cca_training_gaussian  # same
-        d_modeling_mean_error = np.mean(d_cca_training - d_ls_predicted, axis=1).reshape(-1, 1)  # same
-        d_modeling_error = d_cca_training - d_ls_predicted - repmat(d_modeling_mean_error, 1,
-                                                                    np.size(d_cca_training, axis=1))  # same
+        d_ls_predicted = g @ h_cca_training_gaussian  # (n_components_CCA, n_training)
+        d_modeling_mean_error = np.mean(d_cca_training - d_ls_predicted, axis=1).reshape(-1, 1)  # (n_comp_CCA, 1)
+        d_modeling_error = d_cca_training \
+                           - d_ls_predicted \
+                           - repmat(d_modeling_mean_error, 1, np.size(d_cca_training, axis=1))
+        # (n_comp_CCA, n_training)
         # Information about the covariance of the posterior distribution.
-        d_modeling_covariance = (d_modeling_error @ d_modeling_error.T) / n_training  # same
+        d_modeling_covariance = (d_modeling_error @ d_modeling_error.T) / n_training  # (n_comp_CCA, n_comp_CCA)
 
         # Computation of the posterior mean
-        h_mean = np.row_stack(np.mean(h_cca_training_gaussian, axis=1))  # same
+        h_mean = np.row_stack(np.mean(h_cca_training_gaussian, axis=1))  # (n_comp_CCA, 1)
         h_mean = np.where(np.abs(h_mean) < 1e-12, 0, h_mean)  # My mean is 0, as expected.
 
         h_mean_posterior = h_mean \
                            + h_cov_operator @ g.T \
                            @ np.linalg.pinv(g @ h_cov_operator @ g.T + d_noise_covariance + d_modeling_covariance) \
-                           @ (d_cca_prediction.reshape(-1, 1) + d_modeling_mean_error - g @ h_mean)  # same
-
+                           @ (d_cca_prediction.reshape(-1, 1) + d_modeling_mean_error - g @ h_mean)  # (n_comp_CCA, 1)
         # h posterior covariance
         h_posterior_covariance = h_cov_operator \
                                  - (h_cov_operator @ g.T) \
                                  @ np.linalg.pinv(g @ h_cov_operator @ g.T + d_noise_covariance + d_modeling_covariance) \
                                  @ g @ h_cov_operator
 
-        h_posterior_covariance = (h_posterior_covariance + h_posterior_covariance.T) / 2  # same
+        h_posterior_covariance = (h_posterior_covariance + h_posterior_covariance.T) / 2  # (n_comp_CCA, n_comp_CCA)
 
-        self.posterior_mean = h_mean_posterior.T[0]
-        self.posterior_covariance = h_posterior_covariance
+        self.posterior_mean = h_mean_posterior.T[0]  # (n_comp_CCA,)
+        self.posterior_covariance = h_posterior_covariance  # (n_comp_CCA, n_comp_CCA)
 
         return h_mean_posterior.T[0], h_posterior_covariance
 
