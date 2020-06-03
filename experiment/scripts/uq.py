3857340a268a477bd8ba86e6b178a33be61f7f11
Robin <robin.thibaut@ugent.be>
2020-06-03 09:17:12 +0200

    Minor changes

--- experiment/scripts/uq.py
+++ experiment/scripts/uq.py
@@ -1,10 +1,11 @@
 #  Copyright (c) 2020. Robin Thibaut, Ghent University
 
 from experiment.bel.forecast_error import UncertaintyQuantification
+from experiment.processing import decomposition as dcp
 
 if __name__ == '__main__':
-    # dc.bel(n_test=20)
-    uq = UncertaintyQuantification(study_folder='1e77f10f-6bbb-4521-abfb-14be26699f94')
+    dcp.bel(n_test=10)
+    uq = UncertaintyQuantification(study_folder='03f189e4-a7dc-4a09-b1d6-294ff27e7aed')
     for i in range(uq.n_test):
         uq.sample_posterior(sample_n=i, n_posts=500)
         uq.c0(write_vtk=0)
