6685ba1ae7dd98af8e675f23612eb4691e3ae3d6
Robin <robin.thibaut@ugent.be>
2020-06-08 14:31:16 +0200

    Minor changes

--- experiment/scripts/uq.py
+++ experiment/scripts/uq.py
@@ -7,25 +7,20 @@ from experiment.bel.forecast_error import UncertaintyQuantification
 from experiment.base.inventory import Wels
 
 
-def main():
-    wels = Wels()
-
-    perm = wels.combination
-
-    belperm = itertools.permutations(perm, 2)
-
-
-
-
 if __name__ == '__main__':
 
+    wels = Wels()
+    comb = wels.combination
+    belcomb = [list(itertools.combinations(comb, i)) for i in range(2, comb[-1])]
+    belcomb = [item for sublist in belcomb for item in sublist]
+
     try:
-        sf = dcp.bel(test_roots='46933e56d83d4ddcaa26fa0cd8a795db')
+        sf = dcp.bel(test_roots='46933e56d83d4ddcaa26fa0cd8a795db', wel_comb=belcomb)
     except Exception as e:
         print(e)
         shutil.rmtree(sf)
 
-    uq = UncertaintyQuantification(study_folder='271f557f-df78-47aa-90ad-20a949ea2a1b')
+    uq = UncertaintyQuantification(study_folder=sf, wel_comb=belcomb)
     uq.sample_posterior(sample_n=0, n_posts=500)  # Sample posterior
     uq.c0(write_vtk=0)  # Extract 0 contours
     mh = uq.mhd()  # Modified Hausdorff
