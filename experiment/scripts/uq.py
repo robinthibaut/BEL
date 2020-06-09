6fb25d9d6054a84958462aed18f065bf1b1f16f1
Robin <robin.thibaut@ugent.be>
2020-06-09 15:22:42 +0200

    Restructure decomposition.py

--- experiment/scripts/uq.py
+++ experiment/scripts/uq.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
         sf = dcp.bel(test_roots='46933e56d83d4ddcaa26fa0cd8a795db')
     except Exception as e:
         print(e)
-        shutil.rmtree(sf)
+        sf = None
 
-    uq = UncertaintyQuantification(study_folder='271f557f-df78-47aa-90ad-20a949ea2a1b')
+    uq = UncertaintyQuantification(study_folder=sf)
     uq.sample_posterior(sample_n=0, n_posts=500)  # Sample posterior
     uq.c0(write_vtk=0)  # Extract 0 contours
     mh = uq.mhd()  # Modified Hausdorff
