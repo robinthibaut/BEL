d4ae71779f1e41793822361038114452f3d4499e
Robin <robin.thibaut@ugent.be>
2020-06-11 08:37:12 +0200

    Minor changes

--- experiment/scripts/uq.py
+++ experiment/scripts/uq.py
@@ -15,8 +15,29 @@ def combinator(combi):
     return cb
 
 
-if __name__ == '__main__':
+def scan_root(root):
+    wels = Wels()  # Load wels data from base
+    comb = wels.combination  # Get default combination (all)
+    belcomb = [list(itertools.combinations(comb, i)) for i in range(1, comb[-1])]  # Get all possible wel combinations
+    belcomb = [None] + [item for sublist in belcomb for item in sublist]  # Flatten and add None to first compute the
+    # 'base'
 
+    for c in belcomb:
+        try:
+            sf = dcp.bel(test_roots=root, wel_comb=c, base=1)
+        except Exception as e:
+            print(e)
+            shutil.rmtree(sf)
+
+        uq = UncertaintyQuantification(study_folder=sf, wel_comb=c)
+        uq.sample_posterior(sample_n=0, n_posts=500)  # Sample posterior
+        uq.c0(write_vtk=0)  # Extract 0 contours
+        mh = uq.mhd()  # Modified Hausdorff
+        # eb = uq.binary_stack()  # Binary stack
+        # uq.kernel_density()  # Kernel density
+
+
+def scan_roots():
     wels = Wels()  # Load wels data from base
     comb = wels.combination  # Get default combination (all)
 
@@ -38,4 +59,8 @@ if __name__ == '__main__':
             # uq.kernel_density()  # Kernel density
         except Exception as e:
             print(e)
+
+
+if __name__ == '__main__':
+    scan_roots()
 
