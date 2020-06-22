c333f4a9fb712b4de9795e66f3d5acfd11a8d0e8
Robin Thibaut <robin.thibaut@ugent.be>
2020-06-22 11:01:40 +0200

    Test new K

--- experiment/scripts/hydrovision.py
+++ experiment/scripts/hydrovision.py
@@ -1,17 +1,19 @@
 #  Copyright (c) 2020. Robin Thibaut, Ghent University
 
+from experiment.base.inventory import MySetup
 from experiment.goggles.vtkio import ModelVTK
 from experiment.goggles.visualization import Plot
 from experiment.toolbox import filesio
 
 if __name__ == '__main__':
     # VTK
-    mi = ModelVTK('6623dd4fb5014a978d59b9acb03946d2')
-    # mi.flow_vtk()
+    MySetup.Directories.hydro_res_dir = '/Users/robin/OneDrive - UGent/Project-we13c420/experiment/hydro/test'
+    mi = ModelVTK(base=MySetup, folder='macos')
+    mi.flow_vtk()
     # mi.transport_vtk()
-    mi.conc_vtk()
+    # mi.conc_vtk()
     # mi.particles_vtk(path=1)
-    # mi.wels_vtk()
+    mi.wels_vtk()
 
 
 
