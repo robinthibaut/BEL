c5a10b57d4ebebe45138e8f3ca23ff32631bcd11
Robin Thibaut <robin.thibaut@ugent.be>
2020-06-26 18:25:15 +0200

    Compare h1 distribution

--- experiment/scripts/compare_distribution.py
+++ experiment/scripts/compare_distribution.py
@@ -4,10 +4,7 @@ import os
 import joblib
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from experiment.toolbox import utils
-from experiment.toolbox.filesio import load_res, folder_reset
-from experiment.goggles.visualization import Plot, cca_plot, pca_scores, explained_variance
+import seaborn as sns
 from experiment.base.inventory import MySetup
 
 
@@ -35,7 +32,7 @@ sample = '6623dd4fb5014a978d59b9acb03946d2'
 default = ['123456']
 
 dpc_post = dists(sample, folders=default)[0]
-hp1 = dpc_post[:, 0]
+hp1 = dpc_post[:200, 0]
 
 # %%
 
@@ -47,5 +44,27 @@ h_pco = joblib.load(pcaf)
 h1 = h_pco.training_pc[:, 0]
 
 # %%
+# Plot kde distribution
+wm = [h1, hp1]
+colors = ['blue', 'orange']
 
+for i, m in enumerate(wm):
+    sns.kdeplot(m, color=f'{colors[i]}', shade=True, linewidth=2)
+plt.title('First PC distribution of target prior and posterior')
+plt.xlabel('First PC score')
+plt.ylabel('KDE')
+plt.legend(['Prior', 'Posterior'])
+plt.grid(alpha=0.2)
+# plt.savefig(os.path.join(MySetup.Directories.forecasts_dir, f'{sample}_hist.png'), dpi=300, transparent=True)
+plt.show()
 
+sns.distplot(h1, bins=20, kde=False)
+sns.distplot(hp1, bins=20, kde=False)
+plt.title('First PC distribution of target prior and posterior')
+plt.xlabel('First PC score')
+plt.ylabel('Count')
+plt.legend(['Prior', 'Posterior'])
+plt.grid(alpha=0.2)
+# plt.savefig(os.path.join(MySetup.Directories.forecasts_dir, f'{sample}_hist.png'), dpi=300, transparent=True)
+plt.show()
+plt.show()
