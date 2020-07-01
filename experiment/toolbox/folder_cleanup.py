ec68b7d4de9ba2200d897c25eda0fa8c91e29d6f
robinthibaut <50947817+robinthibaut@users.noreply.github.com>
2020-07-01 14:29:15 +0200

    Run simulation

--- experiment/toolbox/folder_cleanup.py
+++ experiment/toolbox/folder_cleanup.py
@@ -3,16 +3,19 @@
 import os
 from os.path import join as jp
 
+from experiment.base.inventory import MySetup
 import experiment.toolbox.filesio as fops
 
 
 def cleanup():
     # cwd = os.getcwd()
-    res_tree = jp('..', 'hydro', 'results')
+    res_tree = MySetup.Directories.hydro_res_dir
     # r=root, d=directories, f = files
-    fops.remove_incomplete(res_tree)
     for r, _, _ in os.walk(res_tree, topdown=False):
         if r != res_tree:
             fops.keep_essential(r)
             fops.remove_bkt(r)
+    fops.remove_incomplete(res_tree)
 
+if __name__ == '__main__':
+    cleanup()
