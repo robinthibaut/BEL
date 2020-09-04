70a6dce1ceefcb3deb68179e25136c420a9e72ec
Robin <robin.thibaut@ugent.be>
2020-09-04 14:01:38 +0200

    Make code command-line runnable

--- experiment/scripts/__init__.py
+++ experiment/scripts/__init__.py
@@ -1,2 +1,16 @@
 #  Copyright (c) 2020. Robin Thibaut, Ghent University
 
+from experiment.base.inventory import MySetup
+from os.path import join, dirname
+import sys
+
+libpath = dirname(dirname(MySetup.Directories.main_dir))
+
+flopath = join(libpath, 'flopy')
+sgpath = join(libpath, 'pysgems')
+
+print(f'flopy located in {flopath}')
+print(f'pysgems located in {sgpath}')
+
+sys.path.append(flopath)
+sys.path.append(sgpath)
