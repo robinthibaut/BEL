1b6aca0a3e52a272253778cd068bc2dc06dba9ad
Robin Thibaut <robin.thibaut@ugent.be>
2020-09-10 09:30:14 +0200

    Minor changes

--- experiment/scripts/forecast_vision.py
+++ experiment/scripts/forecast_vision.py
@@ -12,13 +12,15 @@ def main(roots):
     x_lim, y_lim, grf = fc.x_range, fc.y_range, fc.cell_dim
     mplot = Plot(x_lim=x_lim, y_lim=y_lim, grf=grf, wel_comb=None)
     for sample in roots:
+        print(f'Plotting root {sample}')
         # plot_pc_ba(sample, data=True, target=True)
         # empty_figs(sample)
         wells = ['123456', '1', '2', '3', '4', '5', '6']
         for w in wells:
+            print(f'Plotting well {w}')
             mplot.plot_results(root=sample, folder=w)
         mplot.plot_whpa(sample)
-        # mplot.cca_vision(sample, folders=['123456'])
+        mplot.cca_vision(sample, folders=wells)
         mplot.pca_vision(sample, d=True, h=True, exvar=True, scores=True,
                          folders=wells)
         # ['123456', '1', '2', '3', '4', '5', '6']
