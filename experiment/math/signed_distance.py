3e2e2c62fcaccddfbaf062b229160d569fb1f0da
Robin Thibaut <robin.thibaut@ugent.be>
2020-09-22 11:17:35 +0200

    Minor changes

--- experiment/math/signed_distance.py
+++ experiment/math/signed_distance.py
@@ -5,9 +5,12 @@ import skfmm  # Library to compute the signed distance
 from matplotlib.patches import Polygon
 
 
-def get_centroids(array, grf):
+def get_centroids(array, grf: float):
     """
-    Given a m x n matrix, returns the m x n x 2 matrix of the coordinates of centroids.
+    Given a (m, n) matrix of cells diemsions in the x-y axes, returns the (m, n, 2) matrix of the coordinates of
+    centroids.
+    :param array: (m, n) array
+    :param grf: float: Cell dimension
     """
     xys = np.dstack((np.flip((np.indices(array.shape) + 1), 0) * grf - grf / 2))  # Getting centroids
     return xys.reshape((array.shape[0] * array.shape[1], 2))
@@ -26,24 +29,25 @@ class SignedDistance:
         array = np.ones((self.nrow, self.ncol))  # Dummy array
         self.xys = get_centroids(array, grf) + np.min([x_lim, y_lim], axis=1)  # Centroids of dummy array
 
-    def matrix_poly_bin(self, pzs, outside=-1, inside=1):
+    def matrix_poly_bin(self, pzs, outside: int = -1, inside: int = 1):
         """
         Given a polygon whose vertices are given by the array pzs, and a matrix of
-        centroids coordinates of the surface discretization, assign to the matrix a certain value
+        centroids coordinates of the surface discretization, assigns to the matrix a certain value
         whether the cell is inside or outside said polygon.
+
         To compute the signed distance function, we need a negative/positive value.
 
-        :param pzs: Polygon vertices
+        :param pzs: Polygon vertices (v, 2)
         :param outside: Value to assign to the matrix outside of the polygon
         :param inside: Value to assign to the matrix inside of the polygon
-        :return: phi, the binary matrix
+        :return: phi = the binary matrix
         """
 
         poly = Polygon(pzs, True)  # Creates a Polygon abject out of the polygon vertices in pzs
         ind = np.nonzero(poly.contains_points(self.xys))[0]  # Checks which points are enclosed by polygon.
-        phi = np.ones((self.nrow, self.ncol))*outside  # SD - create matrix of -1
+        phi = np.ones((self.nrow, self.ncol))*outside  # SD - create matrix of 'outside'
         phi = phi.reshape((self.nrow * self.ncol))  # Flatten to have same dimension as 'ind'
-        phi[ind] = inside  # Points inside the WHPA are assigned a value of 1, and 0 for those outside
+        phi[ind] = inside  # Points inside the WHPA are assigned a value of 'inside'
         phi = phi.reshape((self.nrow, self.ncol))  # Reshape
 
         return phi
