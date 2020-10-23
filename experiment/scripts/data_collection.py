b08edaef62e51815739cdf038a054e8e91aaed1f
robinthibaut <50947817+robinthibaut@users.noreply.github.com>
2020-10-23 15:00:27 +0200

    New batch

--- experiment/scripts/data_collection.py
+++ experiment/scripts/data_collection.py
@@ -118,7 +118,6 @@ def main(n_sim: int = None):
 
 if __name__ == "__main__":
     start = time.time()
-#    simulation('818dd775ba0c4e8aa567b5d7d153c9db')
 #    simulation('d8986e2d40064c23b6365041223ad566')
 #    simulation('6de02277697848a8bd093779063d80a6')
     main(1000)
