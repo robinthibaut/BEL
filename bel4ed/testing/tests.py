98e4dd2851249f0d09700ea42365561cccdc2e0b
Robin Thibaut <robin.thibaut@ugent.be>
2021-03-07 12:07:13 +0100

    Changed to pytest

--- bel4ed/testing/tests.py
+++ bel4ed/testing/tests.py
@@ -1,4 +1,3 @@
-import unittest
 from os.path import join as jp
 
 import joblib
@@ -9,49 +8,43 @@ from bel4ed.config import Setup
 from bel4ed.utils import get_roots
 
 
-class TestUQ(unittest.TestCase):
+def test_posterior():
+    """Compare posterior samples with reference default values"""
+    training_file = jp(Setup.Directories.test_dir, "roots.dat")
+    test_file = jp(Setup.Directories.test_dir, "test_roots.dat")
 
-    def test_posterior(self):
-        """Compare posterior samples with reference default values"""
-        training_file = jp(Setup.Directories.test_dir, "roots.dat")
-        test_file = jp(Setup.Directories.test_dir, "test_roots.dat")
+    training_r, test_r = get_roots(training_file=training_file,
+                                   test_file=test_file)
 
-        training_r, test_r = get_roots(training_file=training_file,
-                                       test_file=test_file)
+    wells = [[1, 2, 3, 4, 5, 6]]
 
-        wells = [[1, 2, 3, 4, 5, 6]]
+    test_base = Setup
+    # Switch forecast directory to test directory
+    test_dir = jp(test_base.Directories.test_dir, "forecast")
+    test_base.Directories.forecasts_dir = test_dir
 
-        test_base = Setup
-        # Switch forecast directory to test directory
-        test_dir = jp(test_base.Directories.test_dir, "forecast")
-        test_base.Directories.forecasts_dir = test_dir
+    design.analysis(
+        base=test_base,
+        comb=wells,
+        roots_training=training_r,
+        roots_obs=test_r,
+        wipe=False,
+        flag_base=True,
+    )
 
-        design.analysis(
-            base=test_base,
-            comb=wells,
-            roots_training=training_r,
-            roots_obs=test_r,
-            wipe=False,
-            flag_base=True,
-        )
+    ref_dir = jp(test_base.Directories.ref_dir, "forecast")
 
-        ref_dir = jp(test_base.Directories.ref_dir, "forecast")
+    ref_mean = np.load(jp(ref_dir, test_r[0], "123456", "ref_mean.npy"))
+    ref_covariance = np.load(jp(ref_dir, test_r[0], "123456", "ref_covariance.npy"))
 
-        ref_mean = np.load(jp(ref_dir, test_r[0], "123456", "ref_mean.npy"))
-        ref_covariance = np.load(jp(ref_dir, test_r[0], "123456", "ref_covariance.npy"))
+    test_post = joblib.load(jp(test_dir, test_r[0], "123456", "obj", "post.pkl"))
 
-        test_post = joblib.load(jp(test_dir, test_r[0], "123456", "obj", "post.pkl"))
+    msg1 = "The posterior means are different"
+    np.testing.assert_array_equal(test_post.posterior_mean,
+                                  ref_mean,
+                                  err_msg=msg1)
 
-        msg1 = "The posterior means are different"
-        np.testing.assert_array_equal(test_post.posterior_mean,
-                                      ref_mean,
-                                      err_msg=msg1)
-
-        msg2 = "The posterior covariances are different"
-        np.testing.assert_array_equal(test_post.posterior_covariance,
-                                      ref_covariance,
-                                      err_msg=msg2)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    msg2 = "The posterior covariances are different"
+    np.testing.assert_array_equal(test_post.posterior_covariance,
+                                  ref_covariance,
+                                  err_msg=msg2)
