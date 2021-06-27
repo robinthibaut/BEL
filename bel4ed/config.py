0fe5986ad25ce87a1bb4f6a7355dc41deaa49d2b
Robin Thibaut <robin.thibaut@ugent.be>
2021-06-27 11:04:43 +0200

    Minor changes

--- bel4ed/config.py
+++ bel4ed/config.py
@@ -29,7 +29,7 @@ class Setup:
         main_dir: str = dirname(os.path.abspath(__file__))
         storage_dir: str = join(main_dir, "storage")
         data_dir: str = join(main_dir, "datasets")
-        hydro_res_dir: str = join(data_dir, "forwards")
+        hydro_res_dir: str = join(data_dir, "fwd_structu")
         forecasts_dir: str = join(storage_dir, "forecasts")
         forecasts_base_dir: str = join(forecasts_dir, "base")
         grid_dir: str = join(main_dir, "spatial", "parameters")
