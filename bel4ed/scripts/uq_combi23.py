801fae9d288af82b48ac62b35b1465f22851e868
Robin Thibaut <robin.thibaut@ugent.be>
2021-07-15 14:48:00 +0200

    Minor changes

--- bel4ed/scripts/uq_combi23.py
+++ bel4ed/scripts/uq_combi23.py
@@ -1,44 +1,14 @@
 #  Copyright (c) 2021. Robin Thibaut, Ghent University
 from os.path import join as jp
 
-import multiprocessing as mp
-
-import numpy as np
-import numpy.random
-import pandas as pd
-from loguru import logger
 from skbel.utils import combinator
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import train_test_split
 
-from bel4ed import kernel_bel, init_bel
+from bel4ed import init_bel
 from bel4ed.config import Setup
-from bel4ed.datasets import i_am_root, load_dataset
-from bel4ed.design import bel_training, bel_uq, bel_training_mp
-from bel4ed.goggles import mode_histo
+from bel4ed.datasets import load_dataset
+from bel4ed.design import find_extreme, plot_uq
 from bel4ed.metrics import modified_hausdorff, structural_similarity
-
-
-def plot_uq(
-    metric_function,
-    combi: list = None,
-    directory: str = None,
-    title: str = None,
-    an_i: int = 0,
-):
-    if directory is None:
-        directory = Setup.Directories.forecasts_dir
-    wm = np.load(jp(directory, f"uq_{metric_function.__name__}.npy"))
-    colors = Setup.Wells.colors
-    mode_histo(
-        colors=colors,
-        wm=wm,
-        combi=combi,
-        an_i=an_i,
-        title=title,
-        directory=directory,
-        fig_name=metric_function.__name__,
-    )
-
 
 if __name__ == "__main__":
     combis = combinator([1, 2, 3, 4, 5, 6])
@@ -55,7 +25,6 @@ if __name__ == "__main__":
     X, Y = load_dataset()
 
     random_state = 8872963
-    # random_state = np.random.randint(0, 1e7)
 
     train_size = 1000
     test_size = 250
@@ -76,11 +45,35 @@ if __name__ == "__main__":
         f"{name}_{train_size}_{test_size}_{random_state}",
     )
 
+    train_roots = [t for t in X_train.index]
+
+    with open(
+        jp(
+            Setup.Directories.storage_dir,
+            f"training_roots_{name}_{train_size}_{random_state}.dat",
+        ),
+        "w",
+    ) as doc:
+        for line in train_roots:
+            doc.write(line + "\n")
+    test_roots = [t for t in X_test.index]
+
+    with open(
+        jp(
+            Setup.Directories.storage_dir,
+            f"test_roots_{name}_{test_size}_{random_state}.dat",
+        ),
+        "w",
+    ) as doc:
+        for line in test_roots:
+            doc.write(line + "\n")
+
     args = [
         (bel, X_train, X_test, y_train, y_test, test_directory, c23, tr)
         for tr in test_roots
     ]
-    # n_cpu = 8
+
+    n_cpu = 8
     # pool = mp.Pool(n_cpu)
     # pool.map(bel_training_mp, args)
     # pool.close()
@@ -88,28 +81,31 @@ if __name__ == "__main__":
 
     # Pick metrics
     metrics = (
-        # modified_hausdorff,
+        modified_hausdorff,
         structural_similarity,
     )
     index = X_test.index
+    argsuq = [(bel, y_test, tr, test_directory, c23, metrics) for tr in test_roots]
     # Compute UQ with metrics
-    # bel_uq(
-    #     bel=bel,
-    #     y_obs=y_test,
-    #     index=index,
-    #     directory=test_directory,
-    #     source_ids=c23,
-    #     metrics=metrics,
-    #     delete=True,
-    # )
+    n_cpu = 12
+    # pool = mp.Pool(n_cpu)
+    # pool.map(bel_uq_mp, argsuq)
+    # pool.close()
+    # pool.join()
 
-    [
-        plot_uq(
-            m,
-            directory=test_directory,
-            combi=c23,
-            # title=f"{m.__name__.capitalize()} Training/Test {len(X_train)}/{len(X_test)}",
-            an_i=ix,
-        )
-        for ix, m in enumerate(metrics)
-    ]
+    names = ["MHD", "SSIM"]
+
+    # [
+    #     plot_uq(
+    #         m,
+    #         directory=test_directory,
+    #         combi=c23,
+    #         title=f"{names[ix]} Training/Test {len(X_train)}/{len(X_test)}",
+    #         an_i=ix,
+    #     )
+    #     for ix, m in enumerate(metrics)
+    # ]
+
+    root, comb, mxr, mxc = find_extreme(
+        index, modified_hausdorff, [(2, 6)], test_directory
+    )
